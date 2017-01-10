26fb02951541f59028968cca25ed5b9ea36cc84c
BorisMuzellec <boris.muzellec@polytechnique.edu>
2017-01-10 20:08:49 +0100

    First commit

---
+++ RBM.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jan 10 20:00:30 2017
+
+@author: boris
+"""
+
+class RBM:
+      
+      def __init__(self, num_hidden, num_visible):
+            self.num_hidden = num_hidden
+            self.num_visible = num_visible
+      
+      def train(self, data):
+            pass
